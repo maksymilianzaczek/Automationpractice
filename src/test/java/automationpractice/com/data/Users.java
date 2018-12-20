d3b2d1c94f0887d079d841db6dcc5bad08ca076a
Maksymilianzaczek <maksymilian.zaczek@solsoft.pl>
2018-12-20 15:25:44 +0100

    removed unused lines

--- src/test/java/automationpractice/com/data/Users.java
+++ src/test/java/automationpractice/com/data/Users.java
@@ -11,12 +11,4 @@ public class Users {
 
 
 
-//    public static final User WITH_OBJECT_NUMBER = new User("selenium2", PASSWORD, "selenium2@opoTest.pl");
-//    public static final User WITH_NOT_UNIQUE_EMAIL = new User("selenium4", PASSWORD, "selenium4@test.pl");
-//    public static final User WITH_LOGIN_SAME_AS_NOT_UNIQUE_EMAIL = new User("selenium7@test.pl", PASSWORD, "selenium7@test.pl");
-
-    final private String randomString = RandomStringUtils.randomAlphanumeric(13);
-    final private String randomEmail = String.format("%s@gmai.com", randomString);
-
-
 }
