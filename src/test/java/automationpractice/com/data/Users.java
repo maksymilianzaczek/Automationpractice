5d46e6ac4cb59c91067221d853600826a159c4bd
Maksymilianzaczek <maksymilian.zaczek@solsoft.pl>
2018-12-20 15:26:23 +0100

    removed unused lines

--- src/test/java/automationpractice/com/data/Users.java
+++ src/test/java/automationpractice/com/data/Users.java
@@ -1,14 +1,12 @@
 package automationpractice.com.data;
 
-        import automationpractice.com.domain.User;
-        import org.apache.commons.lang3.RandomStringUtils;
+import automationpractice.com.domain.User;
 
 public class Users {
 
     private static final String PASSWORD = "12345678";
 
     public static final User MACIEJ_STAFIJOWSKI = new User("maciej.stafijowski@solsoft.pl", PASSWORD, "maciej.stafijowski@solsoft.pl");
-
 
 
 }
