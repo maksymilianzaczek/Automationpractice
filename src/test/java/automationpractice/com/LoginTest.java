3165592b0e284f0aab680169a1f5ebd4f0d8816a
Karolina Rakoczy <karolina.rakoczy@solsoft.pl>
2019-02-05 11:03:01 +0100

    Test without login

--- src/test/java/automationpractice/com/LoginTest.java
+++ src/test/java/automationpractice/com/LoginTest.java
@@ -1,11 +1,7 @@
 package automationpractice.com;
 
-import cucumber.api.CucumberOptions;
-import net.serenitybdd.cucumber.CucumberWithSerenity;
-import org.junit.runner.RunWith;
-
-@RunWith(CucumberWithSerenity.class)
-@CucumberOptions(features = "src/test/resources/features/login", plugin = ("json:target/json"))
-public class LoginTest
-{
-}
+//@RunWith(CucumberWithSerenity.class)
+//@CucumberOptions(features = "src/test/resources/features/login", plugin = ("json:target/json"))
+//public class LoginTest
+//{
+//}
